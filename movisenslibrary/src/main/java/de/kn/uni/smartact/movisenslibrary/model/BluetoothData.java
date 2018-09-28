7d2959fc1fde61baac8141f282e68fb8a731fd90
Devender Kumar <deku@comp-pcapek.imm.dtu.dk>
2018-09-28 17:12:33 +0200

    fist upload

---
+++ movisenslibrary/src/main/java/de/kn/uni/smartact/movisenslibrary/model/BluetoothData.java
@@ -0,0 +1,21 @@
+package de.kn.uni.smartact.movisenslibrary.model;
+
+public class BluetoothData {
+    public String date;
+    public String steps;
+    public String met;
+    public String light;
+    public String moderate;
+    public String vigorous;
+    public String count;
+
+    public BluetoothData(String date, String steps, String met, String light, String moderate, String vigorous, String count){
+        this.date = date;
+        this.steps = steps;
+        this.met = met;
+        this.light = light;
+        this.moderate = moderate;
+        this.vigorous = vigorous;
+        this.count = count;
+    }
+}
